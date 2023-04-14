46929d9a489dcf0bc8b6978161b071623b005eb2
cadmann-wong <cadmann@keep.edu.hk>
2023-04-14 10:29:55 +0800

    Updates

---
+++ precache-manifest.804972652751a5688b409bd964db9a39.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a35b3091231d59d6b3dc",
+    "url": "/www/static/css/main.0b187902.chunk.css"
+  },
+  {
+    "revision": "a35b3091231d59d6b3dc",
+    "url": "/www/static/js/main.a35b3091.chunk.js"
+  },
+  {
+    "revision": "bd559390abef1ba0b541",
+    "url": "/www/static/js/1.bd559390.chunk.js"
+  },
+  {
+    "revision": "ab720888b7f38f2d6407",
+    "url": "/www/static/js/runtime~main.ab720888.js"
+  },
+  {
+    "revision": "372dee6df561fd2ed153a8440f42d917",
+    "url": "/www/static/media/bg.372dee6d.png"
+  },
+  {
+    "revision": "c87f155bf71faa272266e56ce01e8a9e",
+    "url": "/www/static/media/ccms-logo-330.c87f155b.jpg"
+  },
+  {
+    "revision": "9fc0c4409c251de00508d25a3e8ad6b2",
+    "url": "/www/static/media/banner-10th.9fc0c440.png"
+  },
+  {
+    "revision": "d1f1e9186f683039a6fefd6afcc5117f",
+    "url": "/www/static/media/mustard-seed.d1f1e918.jpg"
+  },
+  {
+    "revision": "8d2fb495a6fba2ea4cf804c1fc2d2da5",
+    "url": "/www/static/media/spirituality-religion-hands-folded-prayer-holy-bible-church-concept-faith.8d2fb495.jpg"
+  },
+  {
+    "revision": "43cb0cf56940ee145d6a50d6aa06e514",
+    "url": "/www/static/media/four-unrecognizable-people-sitting-around-table-holding-each-other-s-hands-middle.43cb0cf5.jpg"
+  },
+  {
+    "revision": "a8ae2bd2a8557032f2c1dd88beeb9de3",
+    "url": "/www/index.html"
+  }
+];
