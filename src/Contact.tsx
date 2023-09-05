d83f5830cf4b8b74f4c9ca4b78b89347991076d1
cadmann-wong <cadmann@keep.edu.hk>
2023-09-05 13:28:25 +0800

    Update Contact.tsx

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -83,7 +83,7 @@ export default function Contact() {
                     義務傳道　　：賀文亮 傳道<br/>
                     義務助理傳道：孔德承 傳道<br/>
                     義務顧問　　：李潤洪 牧師<br/>
-                    福音幹事　　：何歷同 先生
+                    {/* 福音幹事　　：何歷同 先生 */}
                   </Typography>
                 </CardContent>
               </Card>
