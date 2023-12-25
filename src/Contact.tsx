3dde74a5b5394665329ff8bc53caa99e76acb1e3
cadmann-wong <ckhwong@gmail.com>
2023-12-25 23:53:28 +0800

    Update Contact.tsx

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -81,8 +81,8 @@ export default function Contact() {
                   <Typography variant="subtitle1" color="text.secondary">
                     教會堂主任　：錢國偉 牧師<br/>
                     義務傳道　　：賀文亮 傳道<br/>
-                    義務助理傳道：孔德承 傳道<br/>
                     義務顧問　　：李潤洪 牧師<br/>
+                    義務助理傳道：孔德承 傳道<br/>
                     半職福音幹事：賴康儀 小姐
                   </Typography>
                 </CardContent>
